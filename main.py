e1cb0abef62981dd3829f428290ff69c6a53693a
Jakub Steczkiewicz <77748206+Stekoaa@users.noreply.github.com>
2021-09-16 12:50:25 +0200

    Change code structure
    Minimax algorithm moved to minimax.py and class Board created in board.py

--- main.py
+++ main.py
@@ -1,142 +1,28 @@
-import math
 import random
-import time
+import sys
 
-from pygame.constants import MOUSEBUTTONDOWN
+import pygame
 
-from button import Button
-from constants import *
+import board
+import button
+import constants
+import minimax
+
 
 pygame.init()
 
-
-board = [['' for column in range(BOARD_COLUMNS)] for row in range(BOARD_ROWS)]
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+WIN = pygame.display.set_mode((constants.WIDTH, constants.HEIGHT))
 pygame.display.set_caption('Tic-Tac-Toe')
-
-
-def draw_lines():
-    pygame.draw.line(WIN, LINE_COLOR, (0, CELL_HEIGHT), (WIDTH, CELL_HEIGHT), LINE_WIDTH)
-    pygame.draw.line(WIN, LINE_COLOR, (0, 2 * CELL_HEIGHT + LINE_WIDTH),
-                     (WIDTH, 2 * CELL_HEIGHT + LINE_WIDTH), LINE_WIDTH)
-    pygame.draw.line(WIN, LINE_COLOR, (CELL_WIDTH, 0), (CELL_WIDTH, HEIGHT), LINE_WIDTH)
-    pygame.draw.line(WIN, LINE_COLOR, (2 * CELL_WIDTH + LINE_WIDTH, 0),
-                     (2 * CELL_WIDTH + LINE_WIDTH, HEIGHT), LINE_WIDTH)
-
-
-def draw_vertical_winning_line(col, player):
-    pos_x = col * CELL_WIDTH + CELL_WIDTH // 2
-
-    if player == 'O':
-        color = CIRCLE_COLOR
-    else:
-        color = CROSS_COLOR
-
-    pygame.draw.line(WIN, color, (pos_x, 0), (pos_x, HEIGHT), LINE_WIDTH)
-
-
-def draw_horizontal_winning_line(row, player):
-    pos_y = row * CELL_HEIGHT + CELL_HEIGHT // 2
-
-    if player == 'O':
-        color = CIRCLE_COLOR
-    else:
-        color = CROSS_COLOR
-
-    pygame.draw.line(WIN, color, (0, pos_y), (WIDTH, pos_y), LINE_WIDTH)
-
-
-def draw_diagonal_winning_line(main_diagonal, player):
-    if player == 'O':
-        color = CIRCLE_COLOR
-    else:
-        color = CROSS_COLOR
-
-    if main_diagonal:
-        pygame.draw.line(WIN, color, (25, 25), (WIDTH - 25, HEIGHT - 25), LINE_WIDTH)
-    else:
-        pygame.draw.line(WIN, color, (25, HEIGHT - 25), (WIDTH - 25, 25), LINE_WIDTH)
-
-
-def draw_figures():
-    for row in range(BOARD_ROWS):
-        for column in range(BOARD_COLUMNS):
-            if board[row][column] == 'X':
-                pygame.draw.line(WIN, CROSS_COLOR, (column * CELL_WIDTH + CROSS_OFFSET, row * CELL_HEIGHT + CROSS_OFFSET),
-                                 (column * CELL_WIDTH + CELL_WIDTH - CROSS_OFFSET, row * CELL_HEIGHT + CELL_HEIGHT - CROSS_OFFSET),
-                                 CROSS_WIDTH)
-                pygame.draw.line(WIN, CROSS_COLOR,
-                                 (column * CELL_WIDTH + CROSS_OFFSET, row * CELL_HEIGHT + CELL_HEIGHT - CROSS_OFFSET),
-                                 (column * CELL_WIDTH + CELL_WIDTH - CROSS_OFFSET, row * CELL_HEIGHT + CROSS_OFFSET), CROSS_WIDTH)
-
-            elif board[row][column] == 'O':
-                pygame.draw.circle(WIN, CIRCLE_COLOR, (column * CELL_WIDTH + CELL_WIDTH // 2,
-                                                       row * CELL_HEIGHT + CELL_HEIGHT // 2), CIRCLE_RADIUS,
-                                   CROSS_WIDTH)
-
-
-def insert_letter(row, column, letter):
-    board[row][column] = letter
-
-
-def is_board_full():
-    return not any('' in sublist for sublist in board)
-
-
-def valid_move(row, column):
-    if board[row][column] != '':
-        return False
-    return True
-
-
-def check_win(player):
-    horizontal = check_horizontal_win(player)
-    vertical = check_vertical_win(player)
-    diagonal = check_diagonal_win(player)
-
-    if not horizontal[0] and not vertical[0] and not diagonal[0]:
-        return False
-
-    if horizontal[0]:
-        draw_horizontal_winning_line(horizontal[1], player)
-    elif vertical[0]:
-        draw_vertical_winning_line(vertical[1], player)
-    else:
-        draw_diagonal_winning_line(diagonal[1], player)
-
-    return True
-
-
-def check_vertical_win(player):
-    for column in range(BOARD_COLUMNS):
-        if board[0][column] == player and board[1][column] == player and board[2][column] == player:
-            return True, column
-    return False, ''
-
-
-def check_horizontal_win(player):
-    for row in range(BOARD_ROWS):
-        if board[row][0] == player and board[row][1] == player and board[row][2] == player:
-            return True, row
-    return False, ''
-
-
-def check_diagonal_win(player):
-    if board[0][0] == player and board[1][1] == player and board[2][2] == player:
-        return True, True
-    elif board[0][2] == player and board[1][1] == player and board[2][0] == player:
-        return True, False
-    else:
-        return False, ''
+board = board.Board(WIN)
 
 
 def show_result(winner):
     if winner == 'X' or winner == 'O':
-        result_text = RESULT_FONT.render(winner + " wins!", 1, (255, 255, 255))
+        result_text = constants.RESULT_FONT.render(winner + ' wins!', 1, (255, 255, 255))
     else:
-        result_text = RESULT_FONT.render("Draw", 1, (255, 255, 255))
+        result_text = constants.RESULT_FONT.render('Draw', 1, (255, 255, 255))
 
-    WIN.blit(result_text, (WIDTH // 2 - RESULT_OFFSET_WIDTH, HEIGHT // 2 - RESULT_OFFSET_HEIGHT))
+    WIN.blit(result_text, (constants.WIDTH//2 - constants.RESULT_OFFSET_WIDTH, constants.HEIGHT//2 - constants.RESULT_OFFSET_HEIGHT))
     pygame.display.update()
 
 
@@ -148,11 +34,10 @@ def get_turn(ai, human, turn, player):
 
 
 def make_move(letter, row, column):
+    if board.valid_move(row, column):
+        board.insert_letter(row, column, letter)
 
-    if valid_move(row, column):
-        insert_letter(row, column, letter)
-
-        if check_win(letter):
+        if board.check_win(letter):
             return letter, True
 
     return '', False
@@ -168,94 +53,29 @@ def randomize():
     return (letters[order[0]], order[0]), (letters[order[1]], order[1])
 
 
-def reset_game():
-    global board
-    time.sleep(3.0)
-    pygame.event.clear()
+def wait():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
-    board = [['' for column in range(BOARD_COLUMNS)] for row in range(BOARD_ROWS)]
-
-
-def evaluate(depth, ai, human):
-    if check_vertical_win(ai)[0] or check_horizontal_win(ai)[0] or check_diagonal_win(ai)[0]:
-        return 10 - depth
-    elif check_vertical_win(human)[0] or check_horizontal_win(human)[0] or check_diagonal_win(human)[0]:
-        return -10 + depth
-    return 0
-
-
-def minimax(depth, is_max, ai, human, alpha, beta):
-    score = evaluate(depth, ai, human)
-
-    if score != 0:
-        return score
-
-    if is_board_full():
-        return 0
-
-    if is_max:
-        best = -math.inf
-
-        for row in range(BOARD_ROWS):
-            for column in range(BOARD_COLUMNS):
-                if valid_move(row, column):
-                    board[row][column] = ai
-                    best = max(best, minimax(depth + 1, False, ai, human, alpha, beta))
-
-                    alpha = max(alpha, best)
-                    board[row][column] = ''
-
-                    if best >= beta:
-                        return best
-        return best
-
-    else:
-        best = math.inf
-
-        for row in range(BOARD_ROWS):
-            for column in range(BOARD_COLUMNS):
-                if valid_move(row, column):
-                    board[row][column] = human
-                    best = min(best, minimax(depth + 1, True, ai, human, alpha, beta))
-
-                    board[row][column] = ''
-                    beta = min(beta, best)
-
-                    if best <= alpha:
-                        return best
-        return best
-
-
-def find_best_move(ai, human):
-    best_value = -math.inf
-    best_move = (-1, -1)
-
-    for row in range(BOARD_ROWS):
-        for column in range(BOARD_COLUMNS):
-            if valid_move(row, column):
-                board[row][column] = ai
-                value = minimax(0, False, ai, human, -math.inf, math.inf)
-                board[row][column] = ''
-
-                if value > best_value:
-                    best_value = value
-                    best_move = (row, column)
-
-    return best_move
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                return
 
 
 def menu():
-    WIN.fill(BG_COLOR)
-    ai_button = Button(LINE_COLOR, WIDTH // 2, HEIGHT // 4, 'Computer')
-    player_button = Button(LINE_COLOR, WIDTH // 2, HEIGHT // 2, 'Friend')
-    exit_button = Button(LINE_COLOR, WIDTH // 2, 3 * HEIGHT // 4, 'Exit')
+    WIN.fill(constants.BG_COLOR)
+    ai_button = button.Button(board.line_color, constants.WIDTH // 2, constants.HEIGHT // 4, 'Computer')
+    player_button = button.Button(board.line_color, constants.WIDTH // 2, constants.HEIGHT // 2, 'Friend')
+    exit_button = button.Button(board.line_color, constants.WIDTH // 2, 3*constants.HEIGHT // 4, 'Exit')
 
     clock = pygame.time.Clock()
     intro = True
     result = ''
 
     while intro:
-        clock.tick(FPS)
+        clock.tick(constants.FPS)
 
         ai_button.draw(WIN)
         player_button.draw(WIN)
@@ -267,7 +87,7 @@ def menu():
             if event.type == pygame.QUIT:
                 pygame.quit()
 
-            if event.type == MOUSEBUTTONDOWN:
+            if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     pos = pygame.mouse.get_pos()
 
@@ -284,45 +104,45 @@ def menu():
 
 
 def play_with_friend():
-    WIN.fill(BG_COLOR)
+    WIN.fill(constants.BG_COLOR)
     clock = pygame.time.Clock()
 
     turn = randomize()
     player_one = turn[0][0]
     player_two = turn[1][0]
+
     result = ''
     game_over = False
     player = 0
 
     while not game_over:
-        clock.tick(FPS)
+        clock.tick(constants.FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
 
-            if event.type == MOUSEBUTTONDOWN:
+            if event.type == pygame.MOUSEBUTTONDOWN:
                 pos_y = event.pos[1]
                 pos_x = event.pos[0]
 
-                row = pos_y // CELL_HEIGHT
-                column = pos_x // CELL_WIDTH
+                row = pos_y // board.cell_height
+                column = pos_x // board.cell_width
 
                 result, game_over = make_move(get_turn(player_one, player_two, turn, player), row, column)
                 player += 1
-                if not game_over and is_board_full():
+                if not game_over and board.is_full():
                     game_over = True
                     result = 'draw'
 
-        draw_lines()
-        draw_figures()
+        board.draw_lines()
+        board.draw_figures()
         pygame.display.update()
 
     return result
 
 
 def play_with_ai():
-
-    WIN.fill(BG_COLOR)
+    WIN.fill(constants.BG_COLOR)
     clock = pygame.time.Clock()
 
     turn = randomize()
@@ -333,47 +153,47 @@ def play_with_ai():
     player = 0
 
     while not game_over:
-        clock.tick(FPS)
+        clock.tick(constants.FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
 
             if player % 2 == turn[1][1]:
-                if event.type == MOUSEBUTTONDOWN:
+                if event.type == pygame.MOUSEBUTTONDOWN:
                     if event.button == 1:
                         pos_y = event.pos[1]
                         pos_x = event.pos[0]
 
-                        row = pos_y // CELL_HEIGHT
-                        column = pos_x // CELL_WIDTH
+                        row = pos_y // board.cell_height
+                        column = pos_x // board.cell_width
 
                         result, game_over = make_move(get_turn(ai, human, turn, player), row, column)
                         player += 1
 
             else:
-                move = find_best_move(ai, human)
+                move = minimax.find_best_move(board, ai, human)
                 result, game_over = make_move(get_turn(ai, human, turn, player), move[0], move[1])
                 player += 1
 
-            if not game_over and is_board_full():
+            if not game_over and board.is_full():
                 game_over = True
                 result = 'draw'
 
-        draw_lines()
-        draw_figures()
+        board.draw_lines()
+        board.draw_figures()
         pygame.display.update()
 
     return result
 
 
 def main():
-
     while True:
         result = menu()
         show_result(result)
-        reset_game()
+        wait()
+        board.reset_game()
 
-        WIN.fill(BG_COLOR)
+        WIN.fill(constants.BG_COLOR)
 
 
 if __name__ == "__main__":
